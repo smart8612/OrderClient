e32b0e88d2caa45337151c03d7514c5b64dc3de4
JeongTaek Han <smart8612@gmail.com>
2023-05-29 15:58:58 +0900

    feat: implement menuItem entity

---
+++ Sources/OrderClient/Entities/MenuItem.swift
@@ -0,0 +1,20 @@
+//
+//  MenuItem.swift
+//  OrderClient
+//
+//  Created by JeongTaek Han on 2023/05/29.
+//
+
+import Foundation
+
+
+struct MenuItem {
+    
+    var id: Int
+    var name: String
+    var detailText: String
+    var price: Double
+    var category: Category
+    var imageURL: URL
+    
+}
