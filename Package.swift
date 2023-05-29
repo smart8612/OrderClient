a73dfdc9f5308b319ccbd5e441134e631e6f4d8c
JeongTaek Han <smart8612@gmail.com>
2023-05-29 15:05:26 +0900

    feat: add NetworkKit to target

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OrderClient",
-            dependencies: []),
+            dependencies: ["NetworkKit"]),
         .testTarget(
             name: "OrderClientTests",
             dependencies: ["OrderClient"]),
