ac262b1dec96eddcaa7e0d11609d8535264de08d
JeongTaek Han <smart8612@gmail.com>
2023-05-29 16:22:01 +0900

    feat: define api response decoding models

---
+++ Sources/OrderClient/Models/Network/Response/CategoriesResponse.swift
@@ -0,0 +1,15 @@
+//
+//  CategoriesResponse.swift
+//  OrderClient
+//
+//  Created by JeongTaek Han on 2023/05/29.
+//
+
+import Foundation
+
+
+struct CategoriesResponse: Codable {
+    
+    let categories: [Category]
+    
+}
---
+++ Sources/OrderClient/Models/Network/Response/OrderResponse.swift
@@ -0,0 +1,23 @@
+//
+//  OrderResponse.swift
+//  OrderClient
+//
+//  Created by JeongTaek Han on 2023/05/29.
+//
+
+import Foundation
+
+
+struct OrderResponse {
+    
+    let preperationTime: Int
+    
+}
+
+extension OrderResponse: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case preperationTime = "preparation_time"
+    }
+    
+}
