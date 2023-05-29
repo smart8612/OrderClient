413e25ed3faee02d384aa7b3f3bfd6e1982b4255
JeongTaek Han <smart8612@gmail.com>
2023-05-29 16:25:37 +0900

    feat: define RestaurantAPIFetchable protocol

---
+++ Sources/OrderClient/RestaurantAPIFetchable.swift
@@ -0,0 +1,44 @@
+//
+//  RestaurantAPIFetchable.swift
+//  OrderClient
+//
+//  Created by JeongTaek Han on 2023/05/29.
+//
+
+import Foundation
+import NetworkKit
+
+
+protocol RestaurantAPIFetchable {
+    
+    typealias MinutesToPrepare = Int
+    
+}
+
+extension RestaurantAPIFetchable {
+    
+    func submitOrder(forMenuIDs menuIDs: [Int]) async throws -> MinutesToPrepare {
+        let apiRequest = RestaurantOrderPostAPIRequest(menuIDs: menuIDs)
+        let result = try await NetworkController().send(request: apiRequest)
+        return result.preperationTime
+    }
+    
+    func fetchCategories() async throws -> [Category] {
+        let apiRequest = RestaurantCategoriesGetAPIRequest()
+        let result = try await NetworkController().send(request: apiRequest)
+        return result.categories
+    }
+    
+    func fetchMenuItems(forCategory categoryName: String) async throws -> [MenuItem] {
+        let apiRequest = RestaurantMenuItemsGetAPIRequest(categoryName: categoryName)
+        let result = try await NetworkController().send(request: apiRequest)
+        return result.items
+    }
+    
+    func fetchImage(from url: URL) async throws -> Data {
+        let apiRequest = ImageGetAPIRequest(path: url.absoluteString)
+        let result = try await NetworkController().send(request: apiRequest)
+        return result
+    }
+    
+}
