34efa2c2a1094871a23a805b4ae73532111f8c07
JeongTaek Han <smart8612@gmail.com>
2023-05-29 15:59:51 +0900

    feat: implement Category entity

---
+++ Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  OrderClient
+//
+//  Created by JeongTaek Han on 2023/05/29.
+//
+
+import Foundation
+
+
+struct Category {
+    
+    var id: Int
+    var name: String
+    
+}
