44ec0a590cc40f1fec8cc87510ccf5f22243bf4f
JeongTaek Han <smart8612@gmail.com>
2023-06-14 23:24:45 +0900

    chore: add dependency SwiftLint #2

--- Package.swift
+++ Package.swift
@@ -17,14 +17,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/smart8612/NetworkKit", from: "0.1.0")
+        .package(url: "https://github.com/smart8612/NetworkKit", from: "0.1.0"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.52.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OrderClient",
-            dependencies: ["NetworkKit"]),
+            dependencies: ["NetworkKit"],
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
         .testTarget(
             name: "OrderClientTests",
             dependencies: ["OrderClient"]),
