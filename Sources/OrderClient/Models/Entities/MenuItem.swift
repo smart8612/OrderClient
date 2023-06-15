c4744c0e5ce923c766c9e4d08386608c0a1f5520
JeongTaek Han <smart8612@gmail.com>
2023-06-15 18:08:08 +0900

    docs: write document comment to MenuItem entity #2

--- Sources/OrderClient/Models/Entities/MenuItem.swift
+++ Sources/OrderClient/Models/Entities/MenuItem.swift
@@ -7,15 +7,40 @@
 
 import Foundation
 
+/// A model representing a menu item.
+///
+/// Menu item refers to a list of food options available for customers to order.
+/// It serves as a comprehensive guide for customers to make informed choices
+/// about the dishes and drinks they would like to enjoy during their dining
+/// experience.
 public struct MenuItem {
     
+    /// The unique number of the menu item.
     public var id: Int
+    
+    /// The name of the menu item.
     public var name: String
+    
+    /// The detail description of the menu item.
     public var detailText: String
+    
+    /// The sales price of the menu item.
     public var price: Double
+    
+    /// The category type of the menu item.
     public var category: Category
+    
+    /// The image link of the menu item.
     public var imageURL: URL
     
+    /// Creates menu item with specified food information.
+    /// - Parameters:
+    ///   - id: The unique number of the menu item.
+    ///   - name: The name of the menu item.
+    ///   - detailText: The detail description of the menu item.
+    ///   - price: The sales price of the menu item.
+    ///   - category: The category type of the menu item.
+    ///   - imageURL: The image link of the menu item.
     public init(id: Int, name: String, detailText: String, price: Double, category: Category, imageURL: URL) {
         self.id = id
         self.name = name
