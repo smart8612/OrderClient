af188a88ea87fed49c871deed60c9df9e8c52d63
JeongTaek Han <smart8612@gmail.com>
2023-06-15 17:46:49 +0900

    docs: write comments on Order entity #2

--- Sources/OrderClient/Models/Entities/Order.swift
+++ Sources/OrderClient/Models/Entities/Order.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
+/// A model representing a order.
+///
+/// Order is a customer's specific request for ``MenuItem``'s.
+/// When a customer decides what they would like to eat, they place
+/// an order with ``MenuItem``.
 public struct Order {
     
+    /// The menu items of the order.
+    ///
+    /// A menu items maintains the list of foods which consumer requested.
     public var menuItems: [MenuItem]
     
+    /// Creates a order with the specified menu items.
+    /// - Parameter menuItems: The menu item list of the order
     public init(menuItems: [MenuItem] = []) {
         self.menuItems = menuItems
     }
